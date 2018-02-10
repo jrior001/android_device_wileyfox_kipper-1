Tortel/android_device_wileyfox_kipper

kipper: Switch to permissive

board/kernel.mk
@@ -1,2 +1,4 @@
 # Kernel
 TARGET_KERNEL_CONFIG := lineageos_kipper_defconfig
+BAORD_KERNEL_CMDLINE += androidboot.selinux=permissive
+

jrior001/android_device_wileyfox_kipper-1

Change-Id: Ia4c979c92f6c2c0f6ad384922b6f8d43a2d1b100

board/kernel.mk
@@ -3,3 +3,4 @@ BOARD_DTBTOOL_ARGS := -2
 BOARD_KERNEL_IMAGE_NAME := Image
 BOARD_KERNEL_SEPARATED_DT := true
 TARGET_KERNEL_CONFIG := lineageos_kipper_defconfig
+BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive